6419dcd7c382a1e6be32021fd94426086e465d9f
jazzify <jm1218@hotmail.com>
2025-04-07 22:33:09 -0500

    feature(users): add JWT auth and ME apis

--- humanify_project/extra_settings/rest_framework.py
+++ humanify_project/extra_settings/rest_framework.py
@@ -3,4 +3,7 @@
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "EXCEPTION_HANDLER": "apps.api.exception_handlers.custom_exception_handler",
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
 }
