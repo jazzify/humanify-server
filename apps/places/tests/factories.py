f4d1681fbf6799e7b58a341eaffe6c830942d740
jazzify <jm1218@hotmail.com>
2025-04-24 22:37:10 -0500

    bugfix(humanify): fix testing factories implementations, enhance clean up

--- apps/places/tests/factories.py
+++ apps/places/tests/factories.py
@@ -1,11 +1,10 @@
 import factory
 from django.core.files.base import ContentFile
 from factory.django import DjangoModelFactory
-from faker import Faker
 
+from apps.common.tests import faker
 from apps.places.models import Place, PlaceImage, PlaceTag
-
-fake = Faker()
+from apps.users.tests.factories import BaseUserFactory
 
 
 class PlaceTagFactory(DjangoModelFactory):
@@ -14,7 +13,7 @@ class PlaceTagFactory(DjangoModelFactory):
         django_get_or_create = ("user", "name")
 
     user = factory.SubFactory(BaseUserFactory)
-    name = factory.LazyFunction(lambda: fake.word()[:10])  # Max length is 10
+    name = factory.LazyFunction(lambda: faker.word()[:10])  # Max length is 10
 
 
 class PlaceFactory(DjangoModelFactory):
@@ -23,12 +22,12 @@ class PlaceFactory(DjangoModelFactory):
         skip_postgeneration_save = True
 
     user = factory.SubFactory(BaseUserFactory)
-    name = factory.LazyFunction(lambda: fake.company())
-    city = factory.LazyFunction(lambda: fake.city())
-    description = factory.LazyFunction(lambda: fake.text(max_nb_chars=500))
-    latitude = factory.LazyFunction(lambda: float(fake.latitude()))
-    longitude = factory.LazyFunction(lambda: float(fake.longitude()))
-    favorite = factory.LazyFunction(lambda: fake.boolean())
+    name = factory.LazyFunction(lambda: faker.company())
+    city = factory.LazyFunction(lambda: faker.city())
+    description = factory.LazyFunction(lambda: faker.text(max_nb_chars=500))
+    latitude = factory.LazyFunction(lambda: float(faker.latitude()))
+    longitude = factory.LazyFunction(lambda: float(faker.longitude()))
+    favorite = factory.LazyFunction(lambda: faker.boolean())
 
     @factory.post_generation
     def tags(self, create, extracted, **kwargs):
@@ -48,7 +47,7 @@ class PlaceImageFactory(DjangoModelFactory):
     @factory.lazy_attribute
     def image(self):
         img_content = ContentFile(
-            name=fake.text(max_nb_chars=500),
+            name=faker.text(max_nb_chars=500),
             content=b"GIF87a\x01\x00\x01\x00\x80\x01\x00\x00\x00\x00ccc,\x00"
             b"\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00;",
         )
--- humanify_project/settings/test.py
+++ humanify_project/settings/test.py
@@ -1,4 +1,24 @@
-from humanify_project.settings.base import *
+from humanify_project.settings.base import *  # noqa F403
 
 TESTING = True
 DEBUG = True
+
+# Define a separate media root for tests
+TEST_MEDIA_ROOT = BASE_DIR / "test_media"  # noqa F405
+
+# Override MEDIA_ROOT for tests
+MEDIA_ROOT = TEST_MEDIA_ROOT
+
+# Ensure the default file storage uses the test media root during tests
+STORAGES = {  # noqa F405
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+        "OPTIONS": {
+            "location": MEDIA_ROOT,
+            "base_url": "/test_media/",
+        },
+    },
+    "staticfiles": {  # noqa F405
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
