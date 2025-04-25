83b4d11a8c5651d747691863cdf7ba2d6f5ef4cb
jazzify <jm1218@hotmail.com>
2025-04-25 16:38:56 -0500

    fature(users): add user creation API and service

--- apps/users/apis.py
+++ apps/users/apis.py
@@ -1,13 +1,17 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework import status
+from rest_framework import serializers, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.users.serializers import BaseUserSerializer
+from apps.users.services import user_create
 
 
 class AuthMeAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
     @extend_schema(
         summary="Auth Me",
         responses={status.HTTP_200_OK: BaseUserSerializer},
@@ -15,3 +19,17 @@ class AuthMeAPIView(APIView):
     def get(self, request: Request) -> Response:
         serializer = BaseUserSerializer(request.user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UserCreateApi(APIView):
+    class InputSerializer(serializers.Serializer):
+        email = serializers.EmailField()
+        password = serializers.CharField()
+
+    def post(self, request: Request) -> Response:
+        serializer = self.InputSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user_create(**serializer.validated_data)
+
+        return Response(status=status.HTTP_201_CREATED)
