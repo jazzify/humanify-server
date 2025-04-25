b233991b49c7a39b22440d4ed9204c368928b74f
jazzify <jm1218@hotmail.com>
2025-04-25 16:48:37 -0500

    chore(users): add test cases for user creation service

--- apps/users/tests/factories.py
+++ apps/users/tests/factories.py
@@ -10,10 +10,7 @@ class BaseUserFactory(DjangoModelFactory):
         model = BaseUser
         skip_postgeneration_save = True
 
-    username = factory.Sequence(lambda n: f"user{n}")  # Unique username
     email = factory.LazyFunction(lambda: faker.email())
-    first_name = factory.LazyFunction(lambda: faker.first_name())
-    last_name = factory.LazyFunction(lambda: faker.last_name())
     is_active = True
     is_admin = False
 
