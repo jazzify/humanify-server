1a223a957d0de6a4d98ff7a6e20601cebdcf75d9
jazzify <jm1218@hotmail.com>
2025-05-05 18:58:39 -0500

    chore(places): split place and place_image creation

--- apps/places/apis.py
+++ apps/places/apis.py
@@ -5,36 +5,67 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.places.serializers import PlaceCreateSerializer, PlaceSerializer
-from apps.places.services import create_place, get_all_places_by_user
+from apps.api.serializers import ValidationErrorSerializer
+from apps.places.serializers import (
+    PlaceCreateSerializer,
+    PlaceImageCreateSerializer,
+    PlaceSerializer,
+)
+from apps.places.services import (
+    create_place,
+    create_place_images,
+    get_all_places_by_user,
+)
 
 
-class CreateListPlaceAPIView(APIView):
+class CreatePlaceImageAPI(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        summary="Create place images",
+        request=PlaceImageCreateSerializer,
+        responses={
+            status.HTTP_201_CREATED: None,
+            status.HTTP_400_BAD_REQUEST: ValidationErrorSerializer,
+        },
+    )
+    def post(self, request: Request, place_id: int) -> Response:
+        serializer = PlaceImageCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        create_place_images(
+            place_id=place_id,
+            images=serializer.validated_data["files"],
+        )
+
+        return Response(status=status.HTTP_201_CREATED)
+
+
+class CreateListPlaceAPI(APIView):
     permission_classes = [IsAuthenticated]
 
     @extend_schema(
         summary="Create place",
         request=PlaceCreateSerializer,
-        responses={status.HTTP_201_CREATED: PlaceSerializer},
+        responses={
+            status.HTTP_201_CREATED: None,
+            status.HTTP_400_BAD_REQUEST: ValidationErrorSerializer,
+        },
     )
     def post(self, request: Request) -> Response:
         serializer = PlaceCreateSerializer(data=request.data)
-        if serializer.is_valid():
-            new_place = create_place(
-                user=request.user,
-                name=serializer.validated_data["name"],
-                description=serializer.validated_data["description"],
-                city=serializer.validated_data["city"],
-                latitude=serializer.validated_data["latitude"],
-                longitude=serializer.validated_data["longitude"],
-                tag_names=serializer.validated_data["tags"],
-                images=serializer.validated_data["images"],
-                favorite=serializer.validated_data["favorite"],
-            )
-            return Response(
-                PlaceSerializer(new_place).data, status=status.HTTP_201_CREATED
-            )
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        create_place(
+            user=request.user,
+            name=serializer.validated_data["name"],
+            description=serializer.validated_data["description"],
+            city=serializer.validated_data["city"],
+            latitude=serializer.validated_data["latitude"],
+            longitude=serializer.validated_data["longitude"],
+            tag_names=serializer.validated_data["tags"],
+            favorite=serializer.validated_data["favorite"],
+        )
+        return Response(status=status.HTTP_201_CREATED)
 
     @extend_schema(
         summary="List places",
