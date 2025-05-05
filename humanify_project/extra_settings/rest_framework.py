4d5c572f43a662b114e27762819e91657840f38b
jazzify <jm1218@hotmail.com>
2025-05-05 17:09:12 -0500

    feature(humanify): add support to html forms and fileupload serializer parsers

--- humanify_project/extra_settings/rest_framework.py
+++ humanify_project/extra_settings/rest_framework.py
@@ -6,4 +6,10 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+        "rest_framework.parsers.FormParser",
+        "rest_framework.parsers.MultiPartParser",
+        "rest_framework.parsers.FileUploadParser",
+    ],
 }
