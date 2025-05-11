e888c47be9c94e090622c0bc76eceb32dc312ccc
jazzify <jm1218@hotmail.com>
2025-05-10 19:12:08 -0500

    feature(images): add image processing app

---
+++ apps/images/constants.py
@@ -0,0 +1,8 @@
+from enum import StrEnum, auto
+
+
+# Using auto() with StrEnum results in the lower-cased member name as the value.
+class ImageTransformations(StrEnum):
+    THUMBNAIL = auto()
+    BLUR = auto()
+    BLACK_AND_WHITE = auto()
