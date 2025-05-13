a7d95de24035f829d2fe059ec6cf24c1b56ca4c8
jazzify <jm1218@hotmail.com>
2025-05-13 18:02:14 -0500

    feature(places): add task to process uploaded images

---
+++ apps/places/tests/test_tasks.py
@@ -0,0 +1,24 @@
+from unittest.mock import patch
+
+import pytest
+
+from apps.images.constants import ImageTransformations
+from apps.places.tasks import process_uploaded_images
+
+
+@patch("apps.images.services.ImageTransformationService")
+@pytest.mark.django_db
+def test_process_uploaded_images(mock_image_service):
+    file_path = "path/to/file.jpg"
+    root_folder = "root_folder"
+    parent_folder = "parent_folder"
+
+    process_uploaded_images.enqueue(file_path, root_folder, parent_folder)
+
+    mock_image_service.assert_called_once_with(
+        image_path=file_path,
+        root_folder=root_folder,
+        parent_folder=parent_folder,
+        transformations=[transformation for transformation in ImageTransformations],
+    )
+    mock_image_service.return_value.apply_transformations.assert_called_once()
