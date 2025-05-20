873e5f705cdfc235bb52718d17ead4281240f4a2
jazzify <jm1218@hotmail.com>
2025-05-20 16:59:10 -0500

    feature(images): add chain transformer

--- apps/images/services.py
+++ apps/images/services.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Type
 
 from apps.images.constants import TRANSFORMATIONS_MULTIPROCESS_TRESHOLD
 from apps.images.data_models import ImageTransformationDataClass
@@ -9,6 +10,7 @@ from apps.images.processing.data_models import (
 from apps.images.processing.managers import ImageLocalManager
 from apps.images.processing.transformers import (
     BaseImageTransformer,
+    ImageChainTransformer,
     ImageMultiProcessTransformer,
     ImageSequentialTransformer,
 )
@@ -19,19 +21,27 @@ logger = logging.getLogger(__name__)
 
 def get_local_transformer(
     transformations: list[ImageProcessingTransformationDataClass],
+    is_chain: bool = False,
 ) -> BaseImageTransformer:
-    if len(transformations) < TRANSFORMATIONS_MULTIPROCESS_TRESHOLD:
-        return ImageSequentialTransformer(transformations=transformations)
-    return ImageMultiProcessTransformer(transformations=transformations)
+    transformer: Type[BaseImageTransformer] = ImageSequentialTransformer
+    if is_chain:
+        transformer = ImageChainTransformer
+    elif len(transformations) >= TRANSFORMATIONS_MULTIPROCESS_TRESHOLD:
+        transformer = ImageMultiProcessTransformer
+
+    return transformer(transformations=transformations)
 
 
 def image_local_transform(
     image_path: str,
     transformations: list[ImageTransformationDataClass],
     parent_folder: str,
+    is_chain: bool = False,
 ) -> list[InternalTransformationManagerSave]:
     transformations_data = get_transformation_dataclasses(transformations)
-    transformer = get_local_transformer(transformations=transformations_data)
+    transformer = get_local_transformer(
+        transformations=transformations_data, is_chain=is_chain
+    )
     image_manager = ImageLocalManager(image_path, transformer=transformer)
     image_manager.apply_transformations()
     return image_manager.save(parent_folder=parent_folder)
