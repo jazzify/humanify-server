0edf59ea9674606c457551db862ed0fa7678bdf8
jazzify <jm1218@hotmail.com>
2025-06-21 15:31:05 -0500

    chore(image_processing): update managers tests

--- apps/image_processing/tests/conftest.py
+++ apps/image_processing/tests/conftest.py
@@ -1,8 +1,42 @@
 import pytest
 from PIL import Image as PImage
 
+from apps.image_processing.core.transformations.blur import (
+    ExternalTransformationFiltersBlur,
+    TransformationBlur,
+)
+from apps.image_processing.core.transformations.thumbnail import (
+    ExternalTransformationFiltersThumbnail,
+    TransformationThumbnail,
+)
+from apps.image_processing.core.transformers.base import (
+    InternalImageTransformationDefinition,
+)
+from apps.image_processing.tests.factories import ProcessingImageFactory
+
 
 @pytest.fixture
 def temp_image_file():
     """Creates a temporary image file and returns its path."""
     return PImage.new("RGB", (60, 30), color="red")
+
+
+@pytest.fixture
+def processing_image_base():
+    return ProcessingImageFactory()
+
+
+@pytest.fixture
+def image_transformations():
+    return [
+        InternalImageTransformationDefinition(
+            identifier="BLUR/radius_80",
+            transformation=TransformationBlur,
+            filters=ExternalTransformationFiltersBlur(radius=80),
+        ),
+        InternalImageTransformationDefinition(
+            identifier="THUMBNAIL/size_64",
+            transformation=TransformationThumbnail,
+            filters=ExternalTransformationFiltersThumbnail(size=(64, 64)),
+        ),
+    ]
--- apps/image_processing/tests/test_core/test_managers.py
+++ apps/image_processing/tests/test_core/test_managers.py
@@ -1,62 +1,71 @@
 from unittest.mock import patch
 
 import pytest
-from django.conf import settings
 from PIL import Image as PImage
 
-from apps.image_processing.core.managers.local import (
+from apps.image_processing.core.managers.base import (
     BaseImageManager,
-    ImageLocalManager,
 )
-from apps.image_processing.models import ProcessingImage
-from apps.image_processing.tests.factories import ProcessingImageFactory
+from apps.image_processing.core.managers.local import ImageLocalManager
+from apps.image_processing.core.transformers.base import BaseImageTransformer
+from apps.image_processing.core.transformers.sequential import (
+    ImageSequentialTransformer,
+)
+from apps.image_processing.models import TransformationBatch
 
 
 class ImageProcesingTestManager(BaseImageManager):
     def _get_image(self):
-        return "test_image"
+        return PImage.new("RGB", (60, 30), color="red")
+
+
+class ImageProcessingTestTransformer(BaseImageTransformer):
+    name = "test_transformer"
+
+    def _transform(self, image: PImage.Image):
+        return image
 
 
 @pytest.mark.django_db
-def test_base_image_manager_init():
-    processing_image = ProcessingImageFactory()
-    manager = ImageProcesingTestManager(image=processing_image, transformer=None)
-    assert manager._opened_image == "test_image"
-    assert manager.get_image() == manager._opened_image
+def test_base_image_manager_init(processing_image_base):
+    with patch.object(ImageProcesingTestManager, "_get_image") as mock_get_image:
+        manager = ImageProcesingTestManager(
+            image=processing_image_base, transformer=None
+        )
+        assert manager.get_image() == manager._opened_image
+    mock_get_image.assert_called_once_with()
 
 
 @pytest.mark.django_db
-def test_base_image_manager_apply_transformations():
-    processing_image = ProcessingImageFactory()
-    manager = ImageProcesingTestManager(image=processing_image, transformer=None)
+def test_apply_transformations_without_transformer(processing_image_base):
+    manager = ImageProcesingTestManager(image=processing_image_base, transformer=None)
     with pytest.raises(NotImplementedError):
         manager.apply_transformations()
 
 
-# def test_image_loading():
-#     manager = ImageLocalManager(image_path=)
-#     assert isinstance(manager._opened_image, PImage.Image)
-#     assert manager.get_image() == manager._opened_image
+@pytest.mark.django_db
+def test_apply_transformations_with_transformer(
+    processing_image_base,
+    image_transformations,
+):
+    transformer = ImageSequentialTransformer(transformations=image_transformations)
+    manager = ImageProcesingTestManager(
+        image=processing_image_base, transformer=transformer
+    )
+    transformations_applied = manager.apply_transformations()
+    transformation_batch = TransformationBatch.objects.first()
 
-# def test_apply_transformations_with_transformer(
-#     mock_transformer_with_data, blur_transformation_instance
-# ):
-#     manager = ImageLocalManager(
-#         image_path=, transformer=mock_transformer_with_data
-#     )
-#     transformations_applied = manager.apply_transformations()
+    assert TransformationBatch.objects.count() == 1
+    assert transformation_batch.input_image == processing_image_base
+    assert transformation_batch.transformer == transformer.name
+    assert len(transformations_applied) == len(image_transformations)
+    assert transformations_applied[0].identifier == image_transformations[0].identifier
+    assert isinstance(transformations_applied[0].image, PImage.Image)
 
-#     mock_transformer_with_data.transform.assert_called_once_with(
-#         image=manager._opened_image
-#     )
-#     assert len(transformations_applied) == 1
-#     assert (
-#         transformations_applied[0].identifier
-#         == blur_transformation_instance.identifier
-#     )
-#     assert isinstance(transformations_applied[0].image, PImage.Image)
 
-# def test_apply_transformations_without_transformer():
-#     manager = ImageLocalManager(image_path=, transformer=None)
-#     with pytest.raises(NotImplementedError):
-#         manager.apply_transformations()
+@pytest.mark.django_db
+@patch("apps.image_processing.core.managers.local.PImage")
+def test_local_image_manager_get_image(mock_pimage, processing_image_base):
+    manager = ImageLocalManager(image=processing_image_base)
+    manager.get_image()
+    mock_pimage.open.assert_called_once_with(processing_image_base.file.path)
